04eacaae86bb98de464a6f5f0dc87b2705c8cc81
Sarath S S <sarathss100@gmail.com>
2024-12-10 14:43:53 +0530

    Initial template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = function() {
+
+  return (
+    <div className='text-3xl text-blue-500 underline'>
+      <h1>hkasdfkasdf</h1>
+    </div>
+  );
+};
+
+export default App;
